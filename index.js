a9f3424a7c53beda745980490a6fbd27ce3c3d6b
Marek Zelinka <mzelinka17@gmail.com>
2024-07-23 06:18:06 +0200

    create note

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ let notes = [
   },
 ]
 
+function generateNoteId() {
+  let maxId = notes.length
+    ? Math.max(...notes.map((note) => Number(note.id)))
+    : 0
+  return String(maxId + 1)
+}
+
 express()
+  .use(express.json())
   .get('/', (_req, res) => {
     res.send('<h1>Hello World!</h1>')
   })
@@ -41,5 +49,20 @@ express()
     let id = req.params.id
     notes = notes.filter((note) => note.id !== id)
     res.status(204).end()
+  })
+  .post('/api/notes', (req, res) => {
+    let { content, important } = req.body
+
+    if (!content) {
+      return res.status(400).json({ error: 'content missing' })
+    }
+
+    let note = {
+      content,
+      important: Boolean(important) || false,
+      id: generateNoteId(),
+    }
+    notes = notes.concat(note)
+    res.status(201).json(note)
   })
   .listen(PORT, () => console.log(`Server running on port ${PORT}`))
