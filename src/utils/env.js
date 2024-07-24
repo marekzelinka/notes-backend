77acb6dacb6c23dd15808201ab4749e879310742
Marek Zelinka <mzelinka17@gmail.com>
2024-07-24 16:30:10 +0200

    setup test env

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,9 +1,19 @@
 import invariant from 'tiny-invariant'
 
+const NODE_ENV = process.env.NODE_ENV
+invariant(NODE_ENV, 'Missing NODE_ENV env var')
+
 const PORT = process.env.PORT
 invariant(PORT, 'Missing PORT env var')
 
-const MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.MONGODB_URI
+if (NODE_ENV === 'test') {
+  const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI
+  invariant(TEST_MONGODB_URI, 'Missing TEST_MONGODB_URI env var')
+
+  MONGODB_URI = TEST_MONGODB_URI
+}
+
 invariant(MONGODB_URI, 'Missing MONGODB_URI env var')
 
 export const env = { PORT, MONGODB_URI }
