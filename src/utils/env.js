abea622ea7c95af7dd082facd7efd8b0a9959eeb
Marek Zelinka <mzelinka17@gmail.com>
2024-07-24 18:52:57 +0200

    refactor mongodb uri for test mode

--- src/utils/env.js
+++ src/utils/env.js
@@ -6,14 +6,7 @@ invariant(NODE_ENV, 'Missing NODE_ENV env var')
 const PORT = process.env.PORT
 invariant(PORT, 'Missing PORT env var')
 
-let MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
 invariant(MONGODB_URI, 'Missing MONGODB_URI env var')
-
-if (NODE_ENV === 'test') {
-  const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI
-  invariant(TEST_MONGODB_URI, 'Missing TEST_MONGODB_URI env var')
-
-  MONGODB_URI = TEST_MONGODB_URI
-}
 
 export const env = { NODE_ENV, PORT, MONGODB_URI }
