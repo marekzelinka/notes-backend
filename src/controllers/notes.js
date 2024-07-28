eb061169f1f370debfba41ce0d1d863de046bd6f
Marek Zelinka <mzelinka17@gmail.com>
2024-07-28 15:11:49 +0200

    format

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,25 +1,36 @@
 import express from 'express'
 import { Note } from '../models/note.js'
+import { User } from '../models/user.js'
 
 export const notesRouter = express.Router()
 
 notesRouter.post('/', async (request, response) => {
-  const { content, important } = request.body
+  const { userId, content, important } = request.body
+
+  const user = await User.findById(userId)
+
   const note = new Note({
     content,
     important,
+    user: user.id,
   })
+
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).send(savedNote)
 })
 
 notesRouter.get('/', async (_request, response) => {
   const notes = await Note.find()
+
   response.send(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
   const id = request.params.id
+
   const note = await Note.findById(id)
 
   if (!note) {
@@ -32,23 +43,24 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.put('/:id', async (request, response) => {
   const id = request.params.id
   const { content, important } = request.body
-  const updatedNote = await Note.findByIdAndUpdate(
-    id,
-    {
-      content,
-      important: Boolean(important) || false,
-    },
-    {
-      new: true,
-      runValidators: true,
-      context: 'query',
-    },
-  )
+
+  const updates = {
+    content,
+    important: Boolean(important) || false,
+  }
+  const updatedNote = await Note.findByIdAndUpdate(id, updates, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+
   response.send(updatedNote)
 })
 
 notesRouter.delete('/:id', async (request, response) => {
   const id = request.params.id
+
   await Note.findByIdAndDelete(id)
+
   response.status(204).end()
 })
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,17 +6,21 @@ export const usersRouter = express.Router()
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
   const passwordHash = await hashPassword(password)
   const user = new User({
     username,
     name,
     passwordHash,
   })
+
   const savedUser = await user.save()
+
   response.status(201).send(savedUser)
 })
 
 usersRouter.get('/', async (_request, response) => {
   const users = await User.find()
+
   response.send(users)
 })
