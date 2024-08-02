5ab8093ad4fcc4347a46adee47c84cc2eceeea95
Marek Zelinka <mzelinka17@gmail.com>
2024-08-02 16:28:19 +0200

    format

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -9,23 +9,28 @@ export function requestLogger(request, _response, next) {
 }
 
 export function unknownEndpoint(_request, response) {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({
+    error: 'unknown endpoint',
+  })
 }
 
 export function errorHandler(error, _request, response, next) {
   logError(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({
+      error: 'malformatted id',
+    })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).send({ error: error.message })
+    return response.status(400).send({
+      error: error.message,
+    })
   } else if (error.name === 'MongoServerError' && error.code === 11000) {
     const prop = Object.keys(error.keyValue)[0]
-    const formattedProp = prop[0].toUpperCase() + prop.slice(1)
 
-    return response
-      .status(400)
-      .send({ error: `${formattedProp} must be unique` })
+    return response.status(400).send({
+      error: `${prop} must be unique`,
+    })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).send({
       error: 'token invalid',
