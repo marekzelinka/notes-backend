bc42bc3dd3874a24065f20698acc18da710fd5e0
Marek Zelinka <mzelinka17@gmail.com>
2024-08-19 09:53:40 +0200

    refactor env

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,15 +1,12 @@
 import invariant from 'tiny-invariant'
 
-const NODE_ENV = process.env.NODE_ENV
-invariant(NODE_ENV, 'Missing NODE_ENV env var')
+export const env = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI,
+  SECRET: process.env.SECRET,
+}
 
-const PORT = process.env.PORT
-invariant(PORT, 'Missing PORT env var')
-
-const MONGODB_URI = process.env.MONGODB_URI
-invariant(MONGODB_URI, 'Missing MONGODB_URI env var')
-
-const SECRET = process.env.SECRET
-invariant(SECRET, 'Missing SECRET env var')
-
-export const env = { NODE_ENV, PORT, MONGODB_URI, SECRET }
+for (const [label, value] of Object.entries(env)) {
+  invariant(value, `Missing ${label} env var`)
+}
